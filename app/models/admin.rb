5816805e504dbd9f89910eca4dcfb5e16e79a09b
yuki9326 <ukey.0613@gmail.com>
2021-08-01 12:36:45 +0000

    [Add]Adminモデル

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,4 +1,4 @@
-class Public::EndUser < ApplicationRecord
+class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
