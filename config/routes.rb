3da4e2d533f57ac0e943821e74df1e88d22ebd6c
yuki9326 <ukey.0613@gmail.com>
2021-08-01 16:03:10 +0000

    [Add]homesコントローラー,会員側トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,8 @@ Rails.application.routes.draw do
     registrations: 'public/registrations'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope module: :public do
+    root to: 'homes#top'
+  end
 end
