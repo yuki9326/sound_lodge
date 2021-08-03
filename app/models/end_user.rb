ef2959b5147f6eb5bd58e72e21455fce1bebfa95
yuki9326 <ukey.0613@gmail.com>
2021-08-03 16:54:35 +0000

    [Finish]会員側新規会員登録画面

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -3,4 +3,6 @@ class EndUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum end_user_status: {ミュージシャン:1, お店:2}
 end
