c90d8e1bd9691452e783f45fd736d9c167747ab4
yuki9326 <ukey.0613@gmail.com>
2021-08-11 09:22:09 +0000

    [Update]会員側contactコントローラー

--- app/controllers/public/contacts_controller.rb
+++ app/controllers/public/contacts_controller.rb
@@ -1,2 +1,32 @@
 class Public::ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def confirm
+    @contact = Contact.new(contact_params)
+    if @contact.invalid?
+      render :new
+    end
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContactMailer.send_mail(@contact).deliver_now
+      redirect_to thanks_path
+    else
+      render :new
+    end
+  end
+
+  def thanks
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email,:name,:subject,:mail_body)
+  end
 end
