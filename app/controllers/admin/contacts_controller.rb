170cbac8164d6e65e9b84e85ccde921288b68cb9
yuki9326 <ukey.0613@gmail.com>
2021-08-19 04:01:35 +0000

    [Update]管理者側contactsコントローラー

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,2 +1,32 @@
 class Admin::ContactsController < ApplicationController
+
+  def index
+    @contacts = Contact.page(params[:page]).order(created_at: :desc).per(20)
+  end
+
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(contact_params)
+    @end_user = @contact.end_user
+    ContactMailer.send_when_admin_reply(@end_user, @contact).deliver_now
+    redirect_to admin_contacts_path
+  end
+
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    @contacts = Contact.page(params[:page]).order(created_at: :desc).per(20)
+    @end_users = EndUser.all
+    render :index
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:subject, :mail_body, :reply)
+  end
 end
--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -7,7 +7,7 @@ class Public::ReviewsController < ApplicationController
   @review = Review.new(review_params)
   @review.end_user_id = current_end_user.id
   @review.save
-  redirect_to root_path
+  redirect_to musician_profile_review_path(@review.id)
   end
 
   def show
