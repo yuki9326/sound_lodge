513da737e1fd1db108b3bd1a3af5b432de240e4c
yuki9326 <ukey.0613@gmail.com>
2021-09-06 11:01:18 +0000

    [Add]管理者側店舗レビュー一覧画面のレイアウト

--- app/controllers/admin/shop_reviews_controller.rb
+++ app/controllers/admin/shop_reviews_controller.rb
@@ -1,2 +1,6 @@
 class Admin::ShopReviewsController < ApplicationController
+
+  def index
+    @shop_reviews = ShopReview.where(shop_profile_id: params[:shop_profile_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -44,7 +44,9 @@ Rails.application.routes.draw do
     resources :musician_profiles, only:[:show, :index] do
       resources :musician_reviews, only:[:index]
     end
-    resources :shop_profiles, only:[:show, :index]
+    resources :shop_profiles, only:[:show, :index] do
+      resources :shop_reviews, only:[:index]
+    end
     resources :end_users, only:[:show, :edit, :update]
     resources :contacts, except:[:show, :create, :new]
   end
