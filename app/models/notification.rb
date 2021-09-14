21e2584ee2df893bf10d145f091dec59b93fe9ed
yuki9326 <ukey.0613@gmail.com>
2021-09-14 19:20:44 +0000

    [Change]トップページの通知アイコンと遷移先

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,7 @@ class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   belongs_to :message, optional: true
   has_many :shop_favorites, primary_key: 'shop_favorite_id', foreign_key: 'id'
+  has_many :musician_favorites, primary_key: 'musician_favorite_id', foreign_key: 'id'
+  has_many :shop_reviews, primary_key: 'shop_review_id', foreign_key: 'id'
+  has_many :musician_reviews, primary_key: 'musician_review_id', foreign_key: 'id'
 end
