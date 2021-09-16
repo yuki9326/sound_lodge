c4ce4c85d27c4c4d3bff9d905d34e7f90c490400
yuki9326 <ukey.0613@gmail.com>
2021-09-16 03:15:07 +0000

    [Add]管理者側ミュージシャンプロフィール編集画面

--- app/controllers/admin/musician_profiles_controller.rb
+++ app/controllers/admin/musician_profiles_controller.rb
@@ -9,4 +9,24 @@ class Admin::MusicianProfilesController < ApplicationController
   def index
     @musician_profiles = MusicianProfile.all
   end
+
+  def edit
+    @musician_profile = MusicianProfile.find(params[:id])
+  end
+
+  def update
+    @musician_profile = MusicianProfile.find(params[:id])
+    if @musician_profile.update(musician_profile_params)
+      redirect_to admin_musician_profile_path(@musician_profile.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def musician_profile_params
+    params.require(:musician_profile).permit(:genre, :end_user_id, :review_id, :area, :instrument, :total_member, :is_vocal, :video, :introduction, :price, :rate, :image)
+  end
+
 end
